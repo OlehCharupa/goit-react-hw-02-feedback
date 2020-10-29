c3b572b10255739af37b65e60f8cea546f502bea
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2020-10-29 17:41:01 +0200

    task01

---
+++ src/Components/Statistics/Statistics.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StaticticBtn, StaticticList } from './Statistics.styled'
+
+const Statistics = ({ good, neutral, bad, setValue }) => {
+    let total = good + neutral + bad
+    let positiveFeedback = Math.round((good / total) * 100)
+    return (
+        <section>
+            <h2>Please leave feedback</h2>
+            <StaticticBtn name="good" onClick={setValue}>Good</StaticticBtn>
+            <StaticticBtn name="neutral" onClick={setValue}>Neutral</StaticticBtn>
+            <StaticticBtn name="bad" onClick={setValue}>Bad</StaticticBtn>
+
+            <h2>Statistics</h2>
+
+            {good > 0 ? <StaticticList>
+                <li>Good: {good}</li>
+                <li>Neutral: {neutral}</li>
+                <li>Bad: {bad}</li>
+                <li>Total: {total}</li>
+                <li>Positive feedback: {positiveFeedback ? positiveFeedback : "0"}%</li>
+            </StaticticList> : <p>No feedback given</p>
+                && neutral > 0 ? <StaticticList>
+                        <li>Good: {good}</li>
+                        <li>Neutral: {neutral}</li>
+                        <li>Bad: {bad}</li>
+                        <li>Total: {total}</li>
+                        <li>Positive feedback: {positiveFeedback ? positiveFeedback : "0"}%</li>
+                    </StaticticList> : <p>No feedback given</p>
+                        && bad > 0 ? <StaticticList>
+                            <li>Good: {good}</li>
+                            <li>Neutral: {neutral}</li>
+                            <li>Bad: {bad}</li>
+                            <li>Total: {total}</li>
+                            <li>Positive feedback: {positiveFeedback ? positiveFeedback : "0"}%</li>
+                        </StaticticList> : <p>No feedback given</p>}
+
+        </section>
+    );
+};
+
+export default Statistics;
